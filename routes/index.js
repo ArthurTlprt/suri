b6416b1ce8fdc45c3bd38fc9d00b26a5b73fd850
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-08-10 18:03:17 +0200

    post fonctionne bien

--- routes/index.js
+++ routes/index.js
@@ -8,49 +8,46 @@ var Post = require('../models/post');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  var posts = [];
-
-  Post.find({}, function(err, postsToCopy){
+  Post.find({}, function(err, posts){
     if(err) { console.log(err); }
-    console.log( JSON.parse(JSON.stringify(postsToCopy)) );
-    posts = JSON.parse(JSON.stringify(postsToCopy));
+
+    var content = {
+      title: 'Suricates illuminés',
+      nav: {
+        li1: 'La troupe',
+        li2: 'Actualité',
+        li3: 'Parcours',
+        li4: 'Les comédiens',
+        li5: '(Nous) contacter'
+      },
+      about: {
+        p1: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?',
+        p2: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?',
+        p3: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'
+      },
+      posts: posts,
+      timelines: [
+        {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'},
+        {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'},
+        {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'},
+        {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'},
+        {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'}
+      ],
+      members: [
+        {name: 'Justine Daudon', depiction: 'La meuf trop forte', src: '/images/team/1.jpg', facebook: 'https://www.facebook.com/justine.daudon?fref=ts'},
+        {name: 'Justine Daudon', depiction: 'La meuf trop forte', src: '/images/team/3.jpg', facebook: 'https://www.facebook.com/justine.daudon?fref=ts'},
+        {name: 'Justine Daudon', depiction: 'La meuf trop forte', src: '/images/team/5.jpg', facebook: 'https://www.facebook.com/justine.daudon?fref=ts'},
+        {name: 'Justine Daudon', depiction: 'La meuf trop forte', src: '/images/team/6.jpg', facebook: 'https://www.facebook.com/justine.daudon?fref=ts'}
+      ],
+      facebook: 'https://www.facebook.com/SuricatesIllumines/?fref=ts'
+
+    };
+
+    res.render('index', content);
   });
 
   //console.log(posts);
 
-  var content = {
-    title: 'Suricates illuminés',
-    nav: {
-      li1: 'La troupe',
-      li2: 'Actualité',
-      li3: 'Parcours',
-      li4: 'Les comédiens',
-      li5: '(Nous) contacter'
-    },
-    about: {
-      p1: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?',
-      p2: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?',
-      p3: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'
-    },
-    posts: posts,
-    timelines: [
-      {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'},
-      {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'},
-      {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'},
-      {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'},
-      {title: 'titre', src:'/images/about/1.jpg', text: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?'}
-    ],
-    members: [
-      {name: 'Justine Daudon', depiction: 'La meuf trop forte', src: '/images/team/1.jpg', facebook: 'https://www.facebook.com/justine.daudon?fref=ts'},
-      {name: 'Justine Daudon', depiction: 'La meuf trop forte', src: '/images/team/3.jpg', facebook: 'https://www.facebook.com/justine.daudon?fref=ts'},
-      {name: 'Justine Daudon', depiction: 'La meuf trop forte', src: '/images/team/5.jpg', facebook: 'https://www.facebook.com/justine.daudon?fref=ts'},
-      {name: 'Justine Daudon', depiction: 'La meuf trop forte', src: '/images/team/6.jpg', facebook: 'https://www.facebook.com/justine.daudon?fref=ts'}
-    ],
-    facebook: 'https://www.facebook.com/SuricatesIllumines/?fref=ts'
-
-  };
-
-  res.render('index', content);
 });
 
 router.get('/login', function(req, res, next) {
