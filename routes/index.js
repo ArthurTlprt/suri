8b7945464fc7a7c903edc60e89c0721f4a614299
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-10-13 12:41:02 +0200

    video fonctionne

--- routes/index.js
+++ routes/index.js
@@ -43,7 +43,7 @@ router.get('/', function(req, res, next) {
             p1: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?',
             p2: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?',
             p3: 'lead Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?',
-            src: 'https://www.youtube.com/watch?v=NLE058XnowM'
+            src: '<iframe width="854" height="480" src="https://www.youtube.com/embed/8UVNT4wvIGY" frameborder="0" allowfullscreen></iframe>'
           },
           posts: posts,
           timelines: timelines,
